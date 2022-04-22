5198cb22caff91e01c0a9b2215e3898535ed3050
ahmet yiğit uysal <75635382+ahmetyigit19@users.noreply.github.com>
2022-04-22 04:51:13 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+
+namespace siberodev
+{
+    class Program
+    {
+        private static readonly Stream program;
+
+        static void Main(string[] args)  //burada çıktıyı okutmaya çalıştım
+        {
+
+            string xmltemp = @"temp.xml";
+            XmlTextReader Xmltemp = new XmlTextReader(xmltemp);
+
+            while (Xmltemp.Read())
+            {
+                if (Xmltemp.NodeType == XmlNodeType.Element)
+                {
+                    Console.WriteLine("Etiket: {0} İçerik: {1}", Xmltemp.Name, Xmltemp.ReadElementContentAsString());
+                }
+            }
+
+            Xmltemp.Close();
+
+
+
+            XmlWriter xmlYazici = XmlWriter.Create(xmltemp);  // burada çıktıyı yazdırmaya çalıştım
+
+            xmlYazici.WriteStartDocument();
+
+            xmlYazici.WriteStartElement("etiket");
+            xmlYazici.WriteString("Etiket içeriği");
+            xmlYazici.WriteEndElement();
+
+            xmlYazici.WriteEndDocument();
+            xmlYazici.Close();
+
+
+
+        }
+    }
+}
